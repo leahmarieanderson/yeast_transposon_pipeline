5b8538b7defecfc5f3d363bc7fb6dc706005ef6d
LEAH ANDERSON <leaha3@sage038.grid.gs.washington.edu>
2024-05-15 15:21:18 -0700

    adding code to allow conda activation within shell script

--- transposons_batch.sh
+++ transposons_batch.sh
@@ -10,7 +10,12 @@ NAME=$3
 mkdir ${NAME}
 cd ${NAME}
 
+#activate the environment - these two lines allow it to be activated even in a job queueing system
+CONDA_BASE=$(conda info --base)
+source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate mcclintock
+
+#run mcclintock with your samples
 python3 /net/dunham/vol2/Leah/230127_FTevo/transposons/mcclintock/mcclintock.py \
 	-r /net/dunham/vol2/Leah/230127_FTevo/transposons/mcclintock/test/sacCer2.fasta \
 	-c /net/dunham/vol2/Leah/230127_FTevo/transposons/mcclintock/test/sac_cer_TE_seqs.fasta \
