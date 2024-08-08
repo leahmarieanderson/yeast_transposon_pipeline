4d5cbf59e536fe51fbeab9f67ce834c16e5fff69
Leah Anderson <leaha3@sage037.grid.gs.washington.edu>
2024-08-08 11:46:48 -0700

    more code cleaning

--- tn_batch_runner.py
+++ tn_batch_runner.py
@@ -14,16 +14,19 @@ os.system(f"ls {working_directory}/fastq/*R1_001.fastq.gz > {transposons_dir}/sa
 
 forward_read_list = open(f"{working_directory}/transposons/sample_list.txt", "r")
 
-#iterate through the sample list; run transposon pipeline
-for fastq in forward_read_list:
-	fastq = fastq.strip()
-	fastq_prefix = fastq[:-15]
-	path_names = fastq.split("/")
-	job_name = path_names[-1][:-20]
-	os.system(
-		f"qsub -N {job_name} -wd {working_directory}/transposons "
-		f"-o {working_directory}/outputs -e {working_directory}/errors "
-		f"transposons_batch.sh {fastq} {fastq_prefix}R2_001.fastq.gz {job_name}")
+#open sample list
+with open(f"{transposons_dir}/sample_list.txt", "r") forward_read_list:
+	#iterate through the sample list; run transposon pipeline
+	for fastq in forward_read_list:
+		fastq = fastq.strip()
+		fastq_prefix = fastq[:-15]
+		path_names = fastq.split("/")
+		job_name = path_names[-1][:-20]
+		#submit each individual sample job to cluster with qsub
+		os.system(
+			f"qsub -N {job_name} -wd {working_directory}/transposons "
+			f"-o {working_directory}/outputs -e {working_directory}/errors "
+			f"transposons_batch.sh {fastq} {fastq_prefix}R2_001.fastq.gz {job_name}")
 
 
 
