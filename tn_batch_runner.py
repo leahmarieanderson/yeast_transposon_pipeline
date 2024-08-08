9e172e5075b1a10fb6419986f27d52c861d68ed6
Leah Anderson <leaha3@sage039.grid.gs.washington.edu>
2024-08-08 11:10:53 -0700

    giving the script a working directory with error and output files for better organization

--- tn_batch_runner.py
+++ tn_batch_runner.py
@@ -6,12 +6,10 @@ arg_list = []
 for arg in sys.argv:
 	arg_list.append(arg)
 
-file_path = arg_list[1]
-
-current_dir = os.getcwd()
+working_directory = arg_list[1]
 
 #make the sample list, only forward reads
-os.system("ls " + file_path + "/*R1_001.fastq.gz > sample_list.txt") 
+os.system("ls " + working_directory + "/fastq/*R1_001.fastq.gz > sample_list.txt") 
 
 forward_read_list = open("sample_list.txt", "r")
 
@@ -21,7 +19,10 @@ for fastq in forward_read_list:
 	fastq_prefix = fastq[:-15]
 	path_names = fastq.split("/")
 	job_name = path_names[-1][:-20]
-	os.system(f"qsub -N {job_name} -wd {current_dir} transposons_batch.sh {fastq} {fastq_prefix}R2_001.fastq.gz {job_name}")
+	os.system(
+		f"qsub -N {job_name} -wd {working_directory}/transposons "
+		f"-o {working_directory}/outputs -e {working_directory}/errors "
+		f"transposons_batch.sh {fastq} {fastq_prefix}R2_001.fastq.gz {job_name}")
 
 
 
