0254b8529aed88fc3689992acf53ceed04204848
Leah Anderson <leaha3@grid-head3.grid.gs.washington.edu>
2024-08-13 16:57:49 -0700

    debugging using the same test sample, using --resume

--- transposons_batch.sh
+++ transposons_batch.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 #$ -pe serial 5
-#$ -l mfree=8778M
+#$ -l mfree=16G
 #$ -l h_rt=10:0:0
+#$ -N test2_CP1C 
+#$ -wd /net/dunham/vol2/Leah/transposons_lab_may2024 
+#$ -o /net/dunham/vol2/Leah/transposons_lab_may2024 
+#$ -e /net/dunham/vol2/Leah/transposons_lab_may2024 
 
 FORWARD=$1
 REVERSE=$2
@@ -15,17 +19,14 @@ CONDA_BASE=$(conda info --base)
 source ${CONDA_BASE}/etc/profile.d/conda.sh
 conda activate mcclintock
 
-#debugging: check active environment and paths
-echo "Active environment: $(conda info | grep 'active environment')"
-
-
 #run mcclintock with your samples
-#python3 /net/gs/vol1/home/leaha3/mcclintock/mcclintock.py \
-#	-r /net/gs/vol1/home/leaha3/mcclintock/test/sacCer2.fasta \
-#	-c /net/gs/vol1/home/leaha3/mcclintock/test/sac_cer_TE_seqs.fasta \
-#	-g /net/gs/vol1/home/leaha3/mcclintock/test/reference_TE_locations.gff \
-#	-t /net/gs/vol1/home/leaha3/mcclintock/test/sac_cer_te_families.tsv \
-#	-1 ${FORWARD} \
-#	-2 ${REVERSE} 
+python3 /net/gs/vol1/home/leaha3/mcclintock/mcclintock.py \
+	-r /net/gs/vol1/home/leaha3/mcclintock/test/sacCer2.fasta \
+	-c /net/gs/vol1/home/leaha3/mcclintock/test/sac_cer_TE_seqs.fasta \
+	-g /net/gs/vol1/home/leaha3/mcclintock/test/reference_TE_locations.gff \
+	-t /net/gs/vol1/home/leaha3/mcclintock/test/sac_cer_te_families.tsv \
+	-1 ${FORWARD} \
+	-2 ${REVERSE} \
+    --resume
 
 conda deactivate
